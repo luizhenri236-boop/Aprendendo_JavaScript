624bcf5e0d14b21055eb4ae5936cbb56afc6d844
Luis Dev-junior <luizhenri236@gmail.com>
2026-02-09 05:21:14 -0300

    Aprendendo novas Funções

---
+++ Aulas de JavaScript/Aula 9/Recursiva.js
@@ -0,0 +1,14 @@
+//Recursividade
+function fatorial(n) {
+    if (n==1) {
+        return 1
+    }else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(5))
+// 5! = 5 x 4!
+
+//ou
+
+//n1 = n x (n-1)!
