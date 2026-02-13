d76e12c831e23361dee7429bdf1a29ded0cde823
Luis Dev-junior <luizhenri236@gmail.com>
2026-02-12 22:34:16 -0300

    metado do projeto

--- Aulas de JavaScript/Aula 10ex/ex018/script.js
+++ Aulas de JavaScript/Aula 10ex/ex018/script.js
@@ -1,5 +1,5 @@
 let num = document.querySelector('input#fnun')
-let lista = document.querySelector('input.flista')
+let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
@@ -21,8 +21,32 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        alert('Tudo ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor iválido ou já encontrato na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for (let pos in valores) {
+            if (valores[pos] > maior) {
+                
+            }
+        }
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.`
     }
 }
